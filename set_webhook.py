902cb802df3dff061233af3724ff01982ccb9fc4
imdilxushbek <xasanovdilxusbek1@gmail.com>
2025-04-15 23:38:56 +0500

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,7 +1,15 @@
 import requests
 
 TOKEN = "7739757583:AAEy2UQJdMppYSrOlS0djdwluUWIpzcxjxA"
-URL = "https://xasanovshop-bot.onrender.com/webhook"
+URL = "https://xasanovshop.onrender.com"  # Bu yerga o'z hosting linkingizni yozing
 
-response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={URL}")
-print(response.text)
+def set_webhook():
+    webhook_url = f"{URL}/webhook"
+    response = requests.get(
+        f"https://api.telegram.org/bot{TOKEN}/setWebhook",
+        params={"url": webhook_url}
+    )
+    print(response.json())
+
+if __name__ == "__main__":
+    set_webhook()
